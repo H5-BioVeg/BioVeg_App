b44a2fcba718b6b441321880824c7ab37b54c4fb
NansyT <nanna.tronbjerg@gmail.com>
2023-03-10 09:38:40 +0100

    Update arduino_connector.dart
    Added some comments

--- lib/arduino_connector.dart
+++ lib/arduino_connector.dart
@@ -1,14 +1,18 @@
-import 'package:flutter/foundation.dart';
 import 'package:ping_discover_network_forked/ping_discover_network_forked.dart';
+
 class ArduinoConnector
  {
   String scanForGreenHouses() {
-    final stream = NetworkAnalyzer.discover2('192.168.0', 80, timeout: Duration(milliseconds: 5000));
+    //Pings all network devices at once with discover2()
+    final stream = NetworkAnalyzer.discover2('..', 81, timeout: Duration(milliseconds: 5000));
     stream.listen((NetworkAddress addr) {
+      //Checks to see if the address exists as a device 
       if(addr.exists){
-        
-        DebugPrintCallback debugPrint = debugPrintThrottled;
-        debugPrint(addr.ip);
+        //Print the found devices
+        print(addr.ip);
+      }
+      else {
+        print("No");
       }
     }).onDone(() => print('Done'));
     return "dd";
